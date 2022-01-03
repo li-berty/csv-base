682d07b7ed9fc5eb62bc008ac78fdb4da90baab9
Berty Li <46299378+li-berty@users.noreply.github.com>
2022-01-03 15:04:33 +0300

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,39 @@
+cat  > /etc/nginx/nginx.conf << 'EOF'
+# New nginx.conf
+worker_processes  1;
+events {
+    worker_connections  1024;
+}
+http {
+    include       mime.types;
+    default_type  application/octet-stream;
+    sendfile        on;
+    keepalive_timeout  65;
+    types_hash_max_size 4096;
+    server_names_hash_bucket_size 128;
+    server {
+        listen       80;
+        server_name  localhost;
+		location / {
+		root   /srv/http;
+		index  index.html index.htm index.php;
+		}
+        error_page   500 502 503 504  /50x.html;
+		location = /50x.html {
+		root   /srv/http;
+		}
+		location ~ \.php$ {
+		root           /srv/http;
+		fastcgi_pass   unix:/run/php-fpm/php-fpm.sock;
+		fastcgi_index  index.php;
+		fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;
+		include        fastcgi_params;
+		}
+    }
+}
+EOF
+
+
 alias apt="sudo apt"
 alias q!="exit"
 alias ?="pwd"
