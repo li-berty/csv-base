7e23ee9a3c2c0fd395b8bcbbec4ff302d0b1a451
li-berty <46299378+li-berty@users.noreply.github.com>
2022-04-05 22:03:57 +0300

    glue

--- glue.sh
+++ glue.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# NR			The total number of input records seen so far
-# FNR			The input record number in the current input file
 
-dir=`dirname $0`	# start script directory
-
-awk '(NR == 1) || (FNR > 0)' $dir/*.csv > $dir/output.csv
-echo "DONE! Inspect created $dir/output.csv file."
+dir=`dirname $0`; awk '(NR == 1) || (FNR > 0)' $dir/*.csv > $dir/output.csv; echo "DONE! Inspect created $dir/output.csv file."
